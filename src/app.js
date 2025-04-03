2448a3a42d1622788ff71efa88f33a46b7a70a3f
CrisLMon <cristian_elopezm@soy.sena.edu.co>
2025-04-02 20:14:04 -0500

    configuracion del servidor web en express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log("workout-tracker app");
+const express = require("express"); // Import express
+const app = express(); // Create an instance of express
+const port = 8000; // puerto de escucha
+
+app.listen(port, () => {
+    console.log(`Servidor corriendo en http://localhost:${port}`);
+  });
+
+app.get("/", (req, res) => {
+    res.send("Hola mi server en Express");
+  });
